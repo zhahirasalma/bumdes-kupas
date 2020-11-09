dbece9e138bd534d12e73fd6797e75d4c038c0aa
Zhahira <54302760+zhahirasalma@users.noreply.github.com>
2020-11-09 20:20:42 +0700

    ubah warna dan tambah contoh tabel di modal layanan

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -54,17 +54,17 @@
                                     DAFTAR SEBAGAI BANK SAMPAH</a>
                                 <div class="dropdown-divider"></div>
                                 <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-info">MASUK</a>
-                                <!-- <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-danger">KELUAR</a> -->
+                                    <!-- <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-danger">KELUAR</a> -->
                                 </div>
                             </div>
-                        <div>
+                            <div>
                     </li>
                 </ul>
             </div>
         </div>
     </nav>
     <!-- Masthead-->
-    <header class="masthead bg-primary text-white text-center">
+    <header class="masthead bg-primary text-secondary text-center">
         <div class="container d-flex align-items-center flex-column">
             <!-- Masthead Avatar Image-->
             <img class="masthead-avatar mb-5" src="{{asset('template/assets/img/avataaars.svg')}}" alt="" />
@@ -173,16 +173,75 @@
                             <div class="col-lg-8">
                                 <!-- Portfolio Modal - Title-->
                                 <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"
-                                    id="portfolioModal1Label">Log Cabin</h2>
+                                    id="portfolioModal1Label">History Transaksi</h2>
                                 <!-- Icon Divider-->
                                 <div class="divider-custom">
                                     <div class="divider-custom-line"></div>
-                                    <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
-                                    <div class="divider-custom-line"></div>
                                 </div>
-                                <!-- Portfolio Modal - Image-->
-                                <img class="img-fluid rounded mb-5"
-                                    src="{{asset('template/assets/img/portfolio/cabin.png')}}" alt="" />
+                                <!-- Portfolio Modal - Table-->
+                                <!-- <img class="img-fluid rounded mb-5"
+                                    src="{{asset('template/assets/img/portfolio/cabin.png')}}" alt="" /> -->
+                                <div class="row">
+                                    <div class="col-12">
+                                        <div class="card">
+                                            <div class="card-body">
+                                                
+                                                <div class="table-responsive">
+                                                    <table id="default_table"
+                                                        class="table table-striped table-bordered no-wrap">
+                                                        <thead>
+                                                            <tr>
+                                                                <th>No</th>
+                                                                <th>Tanggal Transaksi</th>
+                                                                <th>Jumlah Transaksi</th>
+                                                                <th>Keterangan</th>
+                                                                <th>4</th>
+                                                                <th>5</th>
+                                                                <th>6</th>
+                                                                <th>7</th>
+                                                                <th>8</th>
+                                                                <th>Aksi</th>
+                                                            </tr>
+                                                        </thead>
+                                                        <tbody>
+
+                                                                <td>1</td>
+                                                                <td>2
+                                                                </td>
+                                                                <td>3</td>
+                                                                <td>4
+                                                                </td>
+                                                                <td>5</td>
+                                                                <td>6
+                                                                </td>
+                                                                <td>
+                                                                    7
+                                                                </td>
+                                                                <td>
+                                                                    8
+                                                                </td>
+                                                                <td>9</td>
+                                                                <td>
+                                                                    <a
+                                                                        class="text-success" data-toggle="tooltip"
+                                                                        data-placement="top"
+                                                                        data-original-title="Edit"><i
+                                                                            class="far fa-edit"></i></a>
+                                                                    <a 
+                                                                        @click="deleteData(item.id)" class="text-danger"
+                                                                        data-toggle="tooltip" data-placement="top"
+                                                                        data-original-title="Delete"><i
+                                                                            class="far fa-trash-alt"></i></a>
+                                                                </td>
+                                                            </tr>
+
+                                                        </tbody>
+                                                    </table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
                                 <!-- Portfolio Modal - Text-->
                                 <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque
                                     assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam
@@ -190,7 +249,7 @@
                                 </p>
                                 <button class="btn btn-primary" data-dismiss="modal">
                                     <i class="fas fa-times fa-fw"></i>
-                                    Close Window
+                                    Tutup Halaman
                                 </button>
                             </div>
                         </div>
