bb64c5767229e4f52f518762dc61af21d75b9fea
Zhahira <54302760+zhahirasalma@users.noreply.github.com>
2020-11-12 20:38:08 +0700

    pembuatan master untuk layout

---
+++ resources/views/home.blade.php
@@ -0,0 +1,194 @@
+@extends('layout.master')
+@section('title')
+    Home
+@endsection
+@section('content')
+
+<!-- Masthead-->
+<header class="masthead bg-primary text-secondary text-center">
+    <div class="container d-flex align-items-center flex-column">
+        <!-- Masthead Avatar Image-->
+        <img class="masthead-avatar mb-5" src="{{asset('template/assets/img/avataaars.svg')}}" alt="" />
+        <!-- Masthead Heading-->
+        <h1 class="masthead-heading text-uppercase mb-0">HALO USER</h1>
+        <!-- Icon Divider-->
+        <div class="divider-custom divider-light">
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Masthead Subheading-->
+        <p class="masthead-subheading font-weight-light mb-0">Alamat</p>
+    </div>
+</header>
+<!-- Portfolio Section-->
+<section class="page-section portfolio" id="portfolio">
+    <div class="container">
+        <!-- Portfolio Section Heading-->
+        <h2 class="page-section-heading text-center text-uppercase text-secondary mb-0">Layanan</h2>
+        <!-- Icon Divider-->
+        <div class="divider-custom">
+            <div class="divider-custom-line"></div>
+        </div>
+        <!-- Portfolio Grid Items-->
+        <div class="row justify-content-center">
+            <!-- Portfolio Item 1-->
+            <div class="col-md-6 col-lg-4 mb-5">
+                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal1">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i
+                                class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="{{asset('template/assets/img/portfolio/cabin.png')}}" alt="" />
+                </div>
+            </div>
+            <!-- Portfolio Item 2-->
+            <div class="col-md-6 col-lg-4 mb-5">
+                <div class="portfolio-item mx-auto" data-toggle="modal" data-target="#portfolioModal2">
+                    <div class="portfolio-item-caption d-flex align-items-center justify-content-center h-100 w-100">
+                        <div class="portfolio-item-caption-content text-center text-white"><i
+                                class="fas fa-plus fa-3x"></i></div>
+                    </div>
+                    <img class="img-fluid" src="{{asset('template/assets/img/portfolio/cake.png')}}" alt="" />
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<!-- Portfolio Modals-->
+<!-- Portfolio Modal 1-->
+<div class="portfolio-modal modal fade" id="portfolioModal1" tabindex="-1" role="dialog"
+    aria-labelledby="portfolioModal1Label" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><i class="fas fa-times"></i></span>
+            </button>
+            <div class="modal-body text-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"
+                                id="portfolioModal1Label">History Transaksi</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Table-->
+                            <!-- <img class="img-fluid rounded mb-5"
+                                    src="{{asset('template/assets/img/portfolio/cabin.png')}}" alt="" /> -->
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="card">
+                                        <div class="card-body">
+
+                                            <div class="table-responsive">
+                                                <table id="default_table"
+                                                    class="table table-striped table-bordered no-wrap">
+                                                    <thead>
+                                                        <tr>
+                                                            <th>No</th>
+                                                            <th>Tanggal Transaksi</th>
+                                                            <th>Jumlah Transaksi</th>
+                                                            <th>Keterangan</th>
+                                                            <th>4</th>
+                                                            <th>5</th>
+                                                            <th>6</th>
+                                                            <th>7</th>
+                                                            <th>8</th>
+                                                            <th>Aksi</th>
+                                                        </tr>
+                                                    </thead>
+                                                    <tbody>
+
+                                                        <td>1</td>
+                                                        <td>2
+                                                        </td>
+                                                        <td>3</td>
+                                                        <td>4
+                                                        </td>
+                                                        <td>5</td>
+                                                        <td>6
+                                                        </td>
+                                                        <td>
+                                                            7
+                                                        </td>
+                                                        <td>
+                                                            8
+                                                        </td>
+                                                        <td>9</td>
+                                                        <td>
+                                                            <a class="text-success" data-toggle="tooltip"
+                                                                data-placement="top" data-original-title="Edit"><i
+                                                                    class="far fa-edit"></i></a>
+                                                            <a @click="deleteData(item.id)" class="text-danger"
+                                                                data-toggle="tooltip" data-placement="top"
+                                                                data-original-title="Delete"><i
+                                                                    class="far fa-trash-alt"></i></a>
+                                                        </td>
+                                                        </tr>
+
+                                                    </tbody>
+                                                </table>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque
+                                assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam
+                                velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.
+                            </p>
+                            <button class="btn btn-primary" data-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Tutup Halaman
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+<!-- Portfolio Modal 2-->
+<div class="portfolio-modal modal fade" id="portfolioModal2" tabindex="-1" role="dialog"
+    aria-labelledby="portfolioModal2Label" aria-hidden="true">
+    <div class="modal-dialog modal-xl" role="document">
+        <div class="modal-content">
+            <button class="close" type="button" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true"><i class="fas fa-times"></i></span>
+            </button>
+            <div class="modal-body text-center">
+                <div class="container">
+                    <div class="row justify-content-center">
+                        <div class="col-lg-8">
+                            <!-- Portfolio Modal - Title-->
+                            <h2 class="portfolio-modal-title text-secondary text-uppercase mb-0"
+                                id="portfolioModal2Label">Tasty Cake</h2>
+                            <!-- Icon Divider-->
+                            <div class="divider-custom">
+                                <div class="divider-custom-line"></div>
+                                <div class="divider-custom-icon"><i class="fas fa-star"></i></div>
+                                <div class="divider-custom-line"></div>
+                            </div>
+                            <!-- Portfolio Modal - Image-->
+                            <img class="img-fluid rounded mb-5"
+                                src="{{asset('template/assets/img/portfolio/cake.png')}}" alt="" />
+                            <!-- Portfolio Modal - Text-->
+                            <p class="mb-5">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Mollitia neque
+                                assumenda ipsam nihil, molestias magnam, recusandae quos quis inventore quisquam
+                                velit asperiores, vitae? Reprehenderit soluta, eos quod consequuntur itaque. Nam.
+                            </p>
+                            <button class="btn btn-primary" data-dismiss="modal">
+                                <i class="fas fa-times fa-fw"></i>
+                                Close Window
+                            </button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/layout/topbar.blade.php
@@ -0,0 +1,42 @@
+<nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
+        <div class="container">
+            <a class="navbar-brand js-scroll-trigger" href="#page-top">KUPAS</a>
+            <button
+                class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
+                type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
+                aria-expanded="false" aria-label="Toggle navigation">
+                Menu
+                <i class="fas fa-bars"></i>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarResponsive">
+                <ul class="navbar-nav ml-auto">
+                    <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger"
+                            href="#portfolio">Layanan</a></li>
+                    <!-- <li class="nav-item mx-0 mx-lg-1"><a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger" href="#about">About</a></li> -->
+                    <li class="nav-item mx-0 mx-lg-1">
+                        <div class="nav-item dropdown">
+                            <a class="nav-link py-3 px-0 px-lg-3 rounded js-scroll-trigger dropdown-toggle"
+                                href="javascript:void(0)" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                <span class="ml-2 d-none d-lg-inline-block"><span>Registrasi</span> <i
+                                        data-feather="chevron-down" class="svg-icon"></i></span>
+                            </a>
+                            <div class="dropdown-menu dropdown-menu-right user-dd animated flipInY">
+                                <a class="dropdown-item" href="javascript:void(0)"><i data-feather="user"
+                                        class="svg-icon mr-2 ml-1"></i>
+                                    DAFTAR SEBAGAI WARGA</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="javascript:void(0)"><i data-feather="settings"
+                                        class="svg-icon mr-2 ml-1"></i>
+                                    DAFTAR SEBAGAI BANK SAMPAH</a>
+                                <div class="dropdown-divider"></div>
+                                <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-info">MASUK</a>
+                                    <!-- <div class="pl-4 p-3"><a href="javascript:void(0)" class="btn btn-sm btn-danger">KELUAR</a> -->
+                                </div>
+                            </div>
+                            <div>
+                    </li>
+                </ul>
+            </div>
+        </div>
+</nav>
