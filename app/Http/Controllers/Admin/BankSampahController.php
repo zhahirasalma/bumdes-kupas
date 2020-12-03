15d64399de5583df8e14918d24f0e4b89711f893
FEBRIANA P P <febriana2018@mail.ugm.ac.id>
2020-12-03 12:41:22 +0700

    fix controller, model bank sampah

--- app/Http/Controllers/Admin/BankSampahController.php
+++ app/Http/Controllers/Admin/BankSampahController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use app\Models\BankSampah;
+use App\Models\BankSampah;
 
 class BankSampahController extends Controller
 {
