112e1c9f6508a87079dc0961896e0861432816e0
FEBRIANA P P <febriana2018@mail.ugm.ac.id>
2021-02-15 14:41:43 +0700

    mengubah fillable dan menambah eloquent alamat

--- app/Models/BankSampah.php
+++ app/Models/BankSampah.php
@@ -10,9 +10,18 @@ class BankSampah extends Model
 {
     use SoftDeletes;
     protected $table='bank_sampah';
-    protected $fillable=['no_telp','kota','kecamatan','desa','dukuh', 'detail_alamat', 'id_users'];
+    protected $fillable=['no_telp','id_kota','id_kecamatan','id_desa','dukuh', 'detail_alamat', 'id_users'];
 
     public function user(){
         return $this->belongsTo('App\Models\User', 'id_users');
+    }
+    public function kota(){
+        return $this->belongsTo('App\Models\Kota', 'id_kota');
+    }
+    public function kecamatan(){
+        return $this->belongsTo('App\Models\Kecamatan', 'id_kecamatan');
+    }
+    public function desa(){
+        return $this->belongsTo('App\Models\Desa', 'id_desa');
     }
 }
