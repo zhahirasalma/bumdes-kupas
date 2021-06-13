cd37cac94016acf2647cc4a9a34bf2e5d3b17e03
zhahirasalma <zhahira.s.r@mail.ugm.ac.id>
2021-06-13 21:00:57 +0700

    logout admin

--- resources/views/backend/layout/topbar.blade.php
+++ resources/views/backend/layout/topbar.blade.php
@@ -8,12 +8,12 @@
                     <img alt="" src="{{asset('/assets/img/brand/favicon.png')}}">
                 </span>
                 <div class="media-body ml-2 d-none d-lg-block">
-                    <span class="mb-0 text-sm  font-weight-bold">USER</span>
+                    <span class="mb-0 text-sm  font-weight-bold">{{ Auth::user()->nama }}</span>
                 </div>
             </div>
         </a>
         <div class="dropdown-menu dropdown-menu-arrow dropdown-menu-right">
-            <a href="/" class="dropdown-item">
+            <a href="/logout" class="dropdown-item">
                 <i class="ni ni-user-run"></i>
                 <span>Logout</span>
             </a>
