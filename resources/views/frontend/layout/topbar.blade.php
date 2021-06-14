76a44ca7cfd62a03d0c3063f5235fc0276d8c15f
zhahirasalma <zhahira.s.r@mail.ugm.ac.id>
2021-06-15 00:03:45 +0700

    edit topbar

--- resources/views/frontend/layout/topbar.blade.php
+++ resources/views/frontend/layout/topbar.blade.php
@@ -1,6 +1,8 @@
 <nav class="navbar navbar-expand-lg bg-secondary text-uppercase fixed-top" id="mainNav">
     <div class="container">
         <a class="navbar-brand js-scroll-trigger" href="/">KUPAS</a>
+        @guest
+        @else
         <button
             class="navbar-toggler navbar-toggler-right text-uppercase font-weight-bold bg-primary text-white rounded"
             type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive"
@@ -8,6 +10,7 @@
             Menu
             <i class="fas fa-bars"></i>
         </button>
+        @endguest
         <div class="collapse navbar-collapse" id="navbarResponsive">
             <ul class="navbar-nav ml-auto">
             @guest
